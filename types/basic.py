ac8a4487b0f36e16ee47dfe23f0f4b3b4f0bbf78
cynic <kurisufag1@gmail.com>
2022-03-10 21:54:49 -0500

    better mitigations

--- types/basic.py
+++ types/basic.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageFilter
 from io import BytesIO
 from base64 import b64encode, b64decode
 import pytesseract
@@ -9,8 +9,29 @@ def kill(input, options = []):
     challenge = b64decode(challenge.encode("ascii"))
     data = BytesIO(challenge)
     img = Image.open(data).convert("L")
+
+    if "removelines" in options:
+        img = img.filter(ImageFilter.SMOOTH_MORE)
+        img = img.convert("L")
+        for i in range(img.size[0]):
+            for j in range(img.size[1]):
+                pixel = img.getpixel((i, j))
+                if pixel < 90:
+                    img.putpixel((i,j), 0)
+                else:
+                    img.putpixel((i,j), 255)
+
+    # img.show()
     solution = pytesseract.image_to_string(img)[0:6]
     if "limitchars" in options:
         solution = solution.lower()
+        reps = {
+            "o": "0",
+            "¢": "c",
+            "s": "5",
+            "t": "f",
+        }
+        for rep in reps.keys():
+            solution = solution.replace(rep, reps[rep])
     # img.show()
     return (solution, input)
